2f6f0c26bd67d877539676fbfa09996548797555
Aboood <abdu3793@stud.kea.dk>
2022-12-19 22:47:38 +0100

    fix links

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 import { animate, scroll, stagger, inView } from "https://cdn.skypack.dev/motion";
+/* global consts */
 const topSection = document.querySelector("#home_sec");
-const middleSection = document.querySelector("#my_work_sec");
-const bottomSetion = document.querySelector("#request");
-
+const buttonProject1 = document.querySelector(".viewP1");
+const buttonProject2 = document.querySelector(".viewP2");
+const buttonProject3 = document.querySelector(".viewP3");
+const buttonProject4 = document.querySelector(".viewP4");
+const buttonProject5 = document.querySelector(".viewP5");
+const buttonAbout = document.querySelector(".view_about");
+const body = document.querySelector("body");
+const overlay = document.querySelector("#overlay");
+const popContainer = document.querySelector("#popup_about, .popup_container");
+const popAbout = document.querySelector("#popup_about");
+const sliderContact = document.querySelector("#contact_slider");
+const closeBtn = document.querySelectorAll(".exit");
+const popParagraph = document.querySelector(".popup_container p");
+const popImg = document.querySelector(".popup_container img");
+const popLink = document.querySelector(".popup_container a");
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   buttons();
@@ -23,65 +36,40 @@ function buttons() {
     });
   });
 }
-
 function AnimateUp() {
   inView([topSection], ({ target }) => {
     animate(target.querySelector("h2"), { x: ["200%", "0%"] }, { duration: 0.8 }, { easing: "ease-out" });
     animate(target.querySelector("p"), { x: ["-150%", "0%"] }, { duration: 0.8 }, { easing: "ease-out" });
   });
 }
-
-function AnimateDown() {
-  inView([topSection], ({ target }) => {
-    animate(target.querySelector("h2"), { x: ["0%", "200%"] }, { duration: 0.8 }, { easing: "ease-out" });
-    animate(target.querySelector("p"), { x: ["0%", "-150%"] }, { duration: 0.8 }, { easing: "ease-out" });
-  });
-}
-/* function AnimatePos3() {
-  inView([middleSection], ({ target }) => {
-    animate(target.querySelector("h2"), { x: ["200%", "0%"] }, { duration: 0.8 }, { easing: "ease-out" });
-    animate(target.querySelector("p"), { x: ["-150%", "0%"] }, { duration: 0.8 }, { easing: "ease-out" });
-  });
-} */
-
 window.addEventListener("scroll", () => {
   const scrolled = window.scrollY;
-
-  console.log(scrolled);
-  let ass = scrolled;
-  if (ass < 10) {
+  let screenY = scrolled;
+  if (screenY < 10) {
     document.querySelector("h1").classList.add("typing");
     document.querySelector("h1").style.opacity = "1";
-
     document.querySelector(".sec_1").style.backgroundColor = "var(--accent-clr-red)";
     document.querySelector(".sec_2").style.backgroundColor = "transparent";
     document.querySelector(".sec_3").style.backgroundColor = "transparent";
   }
-
-  if (ass > 600) {
+  if (screenY > 600) {
     AnimateUp();
     document.querySelector("h1").classList.remove("typing");
     document.querySelector("h1").style.opacity = "0";
-
     document.querySelector(".sec_1").style.backgroundColor = "transparent";
     document.querySelector(".sec_3").style.backgroundColor = "transparent";
     document.querySelector(".sec_2").style.backgroundColor = "var(--accent-clr-red)";
   }
-
-  if (ass > 1100) {
+  if (screenY > 1100) {
     document.querySelector(".sec_1").style.backgroundColor = "transparent";
     document.querySelector(".sec_2").style.backgroundColor = "transparent";
     document.querySelector(".sec_3").style.backgroundColor = "var(--accent-clr-red)";
   }
 });
-
-//
-
 document.addEventListener("keydown", function (event) {
   event.preventDefault();
-
   let scrollDistance = document.documentElement.clientHeight;
-  const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+  const key = event.key;
   switch (key) {
     case "ArrowUp":
       scrollDistance *= -1;
@@ -93,9 +81,7 @@ document.addEventListener("keydown", function (event) {
       window.scrollBy(0, scrollDistance);
   }
 });
-
 const sliders = document.querySelectorAll("[data-carousel-button]");
-
 sliders.forEach((button) => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
@@ -108,56 +94,25 @@ sliders.forEach((button) => {
     delete activeSlide.dataset.active;
   });
 });
-
-const buttonProject1 = document.querySelector(".viewP1");
-const buttonProject2 = document.querySelector(".viewP2");
-const buttonProject3 = document.querySelector(".viewP3");
-const buttonProject4 = document.querySelector(".viewP4");
-
-const buttonAbout = document.querySelector(".view_about");
-const body = document.querySelector("body");
-const overlay = document.querySelector("#overlay");
-const popContainer = document.querySelector("#popup_about, .popup_container");
-/* const popContainer2 = document.querySelector(".popup2");
-const popContainer3 = document.querySelector(".popup3"); */
-
-const popAbout = document.querySelector("#popup_about");
-const sliderContact = document.querySelector("#contact_slider");
-const closeBtn = document.querySelectorAll(".exit");
-const closeArrow = document.querySelector(".bi-arrow-right-square");
-const popHeadline = document.querySelector(".popup_container h3");
-const popParagraph = document.querySelector(".popup_container p");
-const popList = document.querySelectorAll(".popup_container li");
-const popImg = document.querySelector(".popup_container img");
-const popLink = document.querySelector(".popup_container a");
-const logo = document.querySelector(".logo");
-
 document.querySelector(".contact").addEventListener("click", () => {
   sliderContact.style.right = "0%";
 });
 document.querySelector(".bi-arrow-right-square").addEventListener("click", () => {
   sliderContact.style.right = "-200%";
 });
-
 buttonProject1.addEventListener("click", () => {
   popParagraph.innerHTML =
     "The purpose of this project is to recreate an existence website and creating a more user-friendly, responsive and dynamic website. <br> Technologies that have been used are WordPress as database, HTML / CSS as content and styling, JavaScript as a fetching method for the data.";
-
   popImg.src = "./media/images/colors_and_anim_web.png";
   popLink.href = "https://abdulhamidsa.github.io/SILFEN-PROJECT/";
-  //
   popContainer.scrollTo(0, 0);
-
   addOverlay();
 });
-
 buttonProject2.addEventListener("click", () => {
   popParagraph.innerHTML =
     "In this assignment our goal is to create an XD prototype, starting with a low-fidelity XD prototype and to end up with a high-fidelity one. It is a long process where it develops our individual skills in understanding the user experience, and both finding user flows and coming up with a solution for them.";
   popImg.src = "./media/images/responsive_web.png";
   popLink.href = "https://xd.adobe.com/view/0fe2fd8f-6a06-4077-93d3-bffea9a6e7d7-5ab1/?fullscreen&hints=off";
-  popContainer.scrollTo(0, 0);
-
   addOverlay();
 });
 buttonProject3.addEventListener("click", () => {
@@ -165,8 +120,6 @@ buttonProject3.addEventListener("click", () => {
     "In this project the focus was on javaScript. Fetching data from the database and clean it by creating a new prototype (object). The goal was to work as much as possible with functions and passing them as parameters as well. <br> This project only focuses on the functionality of the website, rather than the design and all UX - UI related.";
   popImg.src = "./media/images/fonts_and_ui.png";
   popLink.href = "https://abdulhamidsa.github.io/HogwartHackedList/hogwart_project.html";
-  popContainer.scrollTo(0, 0);
-
   addOverlay();
 });
 buttonProject4.addEventListener("click", () => {
@@ -174,11 +127,14 @@ buttonProject4.addEventListener("click", () => {
     "First semester work. This project is a game that depends heavily on javaScript, and was the first time I worked with the language. The goal was to control the visual aspect of the game and make it interactive when the player press on something, toggling classes on and off was the biggest method to run the game.";
   popImg.src = "./media/images/game_js.png";
   popLink.href = "https://designslayer.dk/theme3/catchMeOutSide/index.html";
-  popContainer.scrollTo(0, 0);
-
   addOverlay();
 });
-
+buttonProject5.addEventListener("click", () => {
+  popParagraph.innerHTML = "The goal in this project is to create a responsive solutions for all devices while also working with colour's hierarchy and fonts ";
+  popImg.src = "./media/images/responsive.png";
+  popLink.href = "https://designslayer.dk/responsive-design/index.html";
+  addOverlay();
+});
 buttonAbout.addEventListener("click", addOverlay);
 function addOverlay() {
   body.style.overflow = "hidden";
@@ -190,13 +146,12 @@ function addOverlay() {
   popAbout.style.visibility = "visible";
   popAbout.style.opacity = "1";
   popAbout.style.height = "70vh";
-  console.log("open pop up");
+  popContainer.scrollTo(0, 0);
+  popAbout.scrollTo(0, 0);
 }
-
 closeBtn.forEach((x) => {
   x.addEventListener("click", closePop);
 });
-
 function closePop() {
   body.style.overflow = "auto";
   overlay.style.visibility = "hidden";
@@ -208,21 +163,3 @@ function closePop() {
   popAbout.style.opacity = "0";
   popAbout.style.height = "0vh";
 }
-
-/* function closePopUp() {
-  closePop.style.visibility = "hidden";
-}
- */
-
-/* let i = -1;
-let speed = 150;
-let text = document.getElementById("typewriter").textContent;
-
-function typeWriter() {
-  if (i < text.length) {
-    document.querySelector("#typewriter").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
- */
